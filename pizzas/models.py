7ba56fef7a6d7530b792a6725c4cdae7612cc185
Alex Green <alexgreen@Alexs-MBP.lan>
2020-05-04 18:22:04 -0500

    Initial Setup (re-started)

---
+++ pizzas/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Topping(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = 'Toppings'
+    
+    def __str__(self):
+        return self.name
