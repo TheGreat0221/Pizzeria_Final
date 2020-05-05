b1c04ad28384885f99df927cdd25a4f7dd3a406e
Alex Green <alexgreen@Alexs-MBP.lan>
2020-05-04 22:25:41 -0500

    Added pizza page with drill down into each pizza's toppings

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -5,6 +5,7 @@ app_name = 'pizzas'
 
 urlpatterns = [
     path('', views.index, name='index'), ## home page
-    #path('Pizzas', views.pizzas, name='pizzas'), ## pizza page
-    #path('Toppings', views.toppings, name='toppings'), ## topping page
+    path('pizzas', views.pizzas, name='pizzas'), ## pizza page
+    path('pizzas/<int:pizza_id>/', views.pizza,name="pizza"), ## drill down into each pizza to see toppings
+    ##path('toppings', views.toppings, name='toppings'), ## topping page
 ]
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Pizza, Topping
 
 # Create your views here.
 
 def index(request):
     """The home page for Pizzeria"""
     return render(request, 'pizzas/index.html')
+
+def pizzas(request):
+    pizzas = Pizza.objects.order_by('date_added')
+    context = {'pizzas':pizzas}
+
+    return render(request, 'pizzas/pizzas.html', context)
+
+def pizza(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+    
+    toppings = pizza.topping_set
+    context = {'pizza':pizza, 'toppings': toppings}
+
+    return render(request, 'pizzas/pizza.html', context)
+
+##def toppings(request):
+##    toppings = Topping.objects.order_by('date_added')
+##    context = {'toppings':toppings}
+##    return render(request, 'pizzas/toppings.html', context)
